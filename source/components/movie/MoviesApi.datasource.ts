7729d2a54aa2bcc04c31160ea4c99a3780db490f
abdomokhtar <abdohossam5@gmail.com>
2019-08-08 20:25:01 +0800

    - fix bug with omdb datasource

--- source/components/movie/MoviesApi.datasource.ts
+++ source/components/movie/MoviesApi.datasource.ts
@@ -1,4 +1,4 @@
-import { HTTPCache, RESTDataSource } from 'apollo-datasource-rest'
+import { HTTPCache, RequestOptions, RESTDataSource } from 'apollo-datasource-rest'
 
 import ObjectHelpers from '../../helpers/object'
 import { IMoviesDataSource } from '../../interfaces'
@@ -7,14 +7,23 @@ class TicketsAPI extends RESTDataSource implements IMoviesDataSource {
   private apiKey = '3b4ea366';
   constructor() {
     super();
-    this.baseURL = `http://www.omdbapi.com/?apikey=${this.apiKey}`
+    this.baseURL = `http://www.omdbapi.com/`
     this.httpCache = new HTTPCache() // https://github.com/apollographql/apollo-server/issues/2240#issuecomment-508516969
   }
 
+  public willSendRequest(request:RequestOptions) {
+    request.params.set('apikey', this.apiKey);
+  }
+
   public async searchMovies(query: { t: string, y?: string }) {
-    const path = `&type=movie&${ObjectHelpers.convertObjectToQueryString(query)}`;
-    const result = await this.get(path);
-    return result && result.Response === 'True' ? result : null;
+    const path = `?type=movie&${ObjectHelpers.convertObjectToQueryString(query)}`;
+    let result = null;
+    try {
+      result = await this.get(path);
+    } catch (err) {
+      console.dir(err);
+    }
+    return result && result.Response === 'True' ? result : null
   }
 
   public getApiKey() {
