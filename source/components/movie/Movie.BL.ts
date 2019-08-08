24ac8894d66a896e05461b06ea159101d6d1716f
abdomokhtar <abdohossam5@gmail.com>
2019-08-08 20:24:33 +0800

    - massage data returned by omdb.

--- source/components/movie/Movie.BL.ts
+++ source/components/movie/Movie.BL.ts
@@ -32,18 +32,22 @@ class MovieBL {
     const cleanedAlternativeTitles = matches
     .map((alternativeTitle:string) => MovieBL.cleanMovieTitle(alternativeTitle))
     possibleTitles.push(...(cleanedAlternativeTitles))
+    console.log(possibleTitles);
     return possibleTitles;
   }
   
   public async populateMovies(): Promise<void> {
     // get tickets without movies
-    const ticketsCursor = await TicketModel.find({ movie: { $exists: false } }).cursor()
+    const ticketsCursor = await TicketModel.find(
+      { movie: { $exists: false } },
+      { title: 1 }
+    ).cursor()
     ticketsCursor.on('data', this.getTicketMovie)
   }
 
   private async getTicketMovie(t: Ticket): Promise<void> {
     const possibleTitles = MovieBL.getPossibleMovieTitles(t.title);
-    // 
+
     for (const title of possibleTitles) {
       const movie = await movieAPI.searchMovies({ t: title })
       if (movie) {
@@ -51,9 +55,27 @@ class MovieBL {
           if (key === 'Response') return movieObj;
 
           const castKeys = ['Writer', 'Actors']
-          movieObj[`${key.charAt(0).toLowerCase()}${key.substr(1)}`] = castKeys.includes(key)
+          // lowercase the key
+          const transformedKey = `${key.charAt(0).toLowerCase()}${key.substr(1)}`;
+          movieObj[transformedKey] = castKeys.includes(key)
             ? transformMovieCast(movie[key])
             : movie[key]
+          
+          switch (transformedKey) {
+            case 'metascore':
+              if (movieObj[transformedKey] === 'N/A') movieObj[transformedKey] = 0
+              break
+            case 'boxOffice':
+            case 'imdbVotes':
+            case 'imdbRating':
+              if (movieObj[transformedKey] === 'N/A') movieObj[transformedKey] = 0
+              if (typeof movieObj[transformedKey] === 'string') movieObj[transformedKey] = movieObj[transformedKey].replace(/\$|,/g, '')
+              break
+            case 'released':
+              if (movieObj[transformedKey] === 'N/A') delete movieObj[transformedKey]
+              break
+            default: break
+          }
 
           return movieObj
         }, {})
